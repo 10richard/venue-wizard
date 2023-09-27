b37b4f40d59d20623c00b5c1c4161c1b4288e60b
Richard Ten <rten04@icloud.com>
2023-09-27 19:31:02 -0400

    Add Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#27005D",
+        secondary: "#9400FF",
+        blueshit: "#AED2FF",
+        moreblueshit: "#E4F1FF"
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
