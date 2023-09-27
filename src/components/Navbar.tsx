dcd4160bc6432422ab7680626c7437155769de8c
Richard Ten <rten04@icloud.com>
2023-09-27 08:48:43 -0400

    Create initial navbar
    * Have to refactor/group CSS classes
    * Update brand logo
    * Add hamburger menu
    * Make it prettier?

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="navbar flex justify-between">
+    <nav className="navbar sticky flex justify-between bg-primary text-white items-center w-full">
       <div className="navbar__logo flex items-center">
         <img
           src="/wizard-favicon.png"
@@ -10,13 +10,15 @@ const Navbar = () => {
         <h1 className="text-2xl">VenueWizard</h1>
       </div>
       <ul className="navbar__links flex">
-        <li>Discover</li>
-        <li>Our Promise</li>
-        <li>Pricing</li>
+        <li className="-ml-18 cursor-pointer">Discover</li>
+        <li className="mx-10 cursor-pointer">Our Promise</li>
+        <li className="cursor-pointer">Pricing</li>
       </ul>
       <div className="navbar__buttons">
-        <button>Log In</button>
-        <button>Sign Up</button>
+        <button className="border bg-secondary py-1.5 px-3 rounded-xl">
+          Log In
+        </button>
+        <button className="ml-8 mr-10">Sign Up</button>
       </div>
     </nav>
   );
