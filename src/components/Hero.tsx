856ca729949092eb1442fb87250d0fe70765fd49
Richard Ten <rten04@icloud.com>
2023-10-15 13:41:28 -0400

    Edit Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,20 @@
-import hero from "../assets/hero-img.png";
+import heroTickets from "../assets/hero-img.png";
+import heroBackground from "../assets/hero-bg.png";
 
 const Hero = () => {
   return (
-    <section className="hero-section h-[95vh] bg-lightblue py-10 flex justify-center">
-      <div className="hero-content flex justify-center items-center max-[1060px]:flex-col max-[1060px]:mt-36 w-[80%]">
+    <section className="hero-section h-[85vh] bg-lightblue pt-8 flex justify-center relative">
+      <div className="hero-content flex items-center max-[1300px]:justify-around max-[1300px]:flex-col max-[1300px]:mt-36 w-[80%]">
+        <img
+          className="absolute top-0 right-0 h-full rounded-l-[300px] max-[1300px]:hidden"
+          src={heroBackground}
+          alt=""
+        />
         <div className="hero-content__text font-bold flex flex-col -mt-36">
-          <p className="max-[1060px]:hidden text-xl mb-5">
+          <p className="max-[1300px]:text-lg text-xl mb-5">
             Buy and sell with confidence
           </p>
-          <h1 className="max-[1060px]:text-3xl text-5xl leading-[1.25]">
+          <h1 className="max-[1300px]:text-3xl text-5xl leading-[1.25]">
             Unlock the{" "}
             <a
               target="_blank"
@@ -20,7 +26,7 @@ const Hero = () => {
             <br />
             of Tickets
           </h1>
-          <p className="opacity-60 max-[1060px]:text-xs text-sm mt-8">
+          <p className="opacity-60 max-[1300px]:text-xs text-sm mt-8">
             Experience live events, whether it's a heart-pounding concert,{" "}
             <br className="max-[465px]:hidden" /> a captivating theatre
             production, or a thrilling sports match
@@ -29,9 +35,9 @@ const Hero = () => {
 
         <div className="hero-content__img">
           <img
-            className="w-[550px] min-[1060px]:ml-20 max-[1060px]:w-[350px] max-[1060px]:mt-10"
-            src={hero}
-            alt="Hero banner"
+            className="min-[1300px]:hidden w-[350px] mt-8"
+            src={heroTickets}
+            alt="Pair of 3D tickets"
           />
         </div>
       </div>
