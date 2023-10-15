1cb5df39cceeab97b98b97aa96255fd005a3a0d4
Richard Ten <rten04@icloud.com>
2023-10-14 22:38:41 -0400

    Finish building Mobile Download and Pricing sections

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -1,9 +1,57 @@
-import React from "react";
+import mobileApp from "../assets/mobile-app.png";
+import googlePlayBadge from "../assets/mobile-app-badges/google-play-badge.png";
+import applePlayBadge from "../assets/mobile-app-badges/apple-store-badge.png";
 
 const Download = () => {
   return (
-    <section>Download App - Discover your favorite events on the go</section>
+    <section className="flex justify-center pt-16 mb-20">
+      <div className="flex justify-around w-11/12 max-[1000px]:flex-col max-[1000px]:justify-center max-[1000px]:items-center max-[1000px]:gap-16 bg-secondary/80 rounded-3xl px-10 pt-12">
+        {/* Left side  */}
+        <div className="flex flex-col gap-12 max-[900px]:items-center">
+          <div className="w-[490px] max-[900px]:w-[350px] max-[900px]:text-center text-white">
+            <h1 className="text-4xl font-bold mb-2">Download our App</h1>
+            <p className="opacity-80 max-[900px]:text-sm">
+              Easily secure tickets on the go, get timely event reminders, and
+              unlock special in-app perks by downloading our mobile app today
+            </p>
+          </div>
+          <div className="mobile-app-badges flex max-[900px]:flex-col max-[900px]:gap-2 gap-6">
+            <img
+              className="w-[225px] max-[900px]:w-[200px] cursor-pointer"
+              src={googlePlayBadge}
+              alt="Google play store badge"
+            />
+            <img
+              className="w-[225px] max-[900px]:w-[200px] cursor-pointer"
+              src={applePlayBadge}
+              alt="Apple store badge"
+            />
+          </div>
+        </div>
+
+        {/* Right side */}
+        <img
+          className="max-[900px]:w-[300px] w-[350px]"
+          src={mobileApp}
+          alt="Venue Wizard Mobile App"
+        />
+      </div>
+
+      {/* <div className="flex justify-center items-center w-5/6">
+        <h1 className="text-3xl font-bold w-[600px]">
+          Your Favorite Events, Just a Tap Away
+        </h1>
+        <div>
+          <img
+            className="w-[450px]"
+            src={mobileApp}
+            alt="Venue Wizard Application for Mobile Devices"
+          />
+        </div>
+      </div> */}
+    </section>
   );
+  //Similar to stubhub
 };
 
 export default Download;
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,10 +1,48 @@
-import React from "react";
+import pricingIcon from "../assets/best-price-icon.svg";
 
 const Pricing = () => {
   return (
-    <div>
-      Fee-Free for Buyers - tired of hidden fees stacking on each other, etc.
-    </div>
+    <section
+      id="pricing"
+      className="flex justify-center items-center pt-32 pb-20 bg-lightblue mb-10"
+    >
+      <div className="flex max-[1000px]:flex-col max-[1000px]:items-center max-[1000px]:gap-20 w-5/6 justify-around">
+        <div className="flex flex-col gap-4 max-[1000px]:w-[325px] w-[450px] pt-8 max-[1000px]:text-center">
+          <h1 className="max-[1000px]:text-4xl text-5xl font-bold">
+            Savings Without Sneaky Changes
+          </h1>
+          <p className="opacity-70">
+            We believe in honest pricing. That's why we offer fully-transparent
+            rates with no surprises at checkout, leaving you with more money in
+            your pocket.
+          </p>
+        </div>
+
+        <img
+          className="w-[300px] bg-babyblue rounded-full px-10 pb-10 pt-20"
+          src={pricingIcon}
+          alt="Best price icon"
+        />
+      </div>
+
+      {/* Add slanted bg color
+      <div className="w-5/6 flex max-[1110px]:gap-16 justify-around items-center">
+        <div className="min-[1110px]:w-1/3 min-[1110px]:self-start max-[1110px]:text-center w-5/6">
+          <h1 className="text-4xl font-bold mb-6">Tired of hidden fees?</h1>
+          <h1 className="max-[1110px]:text-3xl text-4xl font-bold mb-6">
+            Savings Without Sneaky Charges.
+          </h1>
+          <p className="text-lg max-[1300px]:text-sm">
+            We believe in honest pricing. That's why we offer fully-transparent
+            rates with no surprises at checkout, leaving you with more money in
+            your pocket.
+          </p>
+        </div>
+        Change color of coin from black to pink or some shit,
+        maybe incorporate different shades
+        <img className="" src={pricingIcon} alt="Pricing Icon" />
+      </div> */}
+    </section>
   );
 };
 
